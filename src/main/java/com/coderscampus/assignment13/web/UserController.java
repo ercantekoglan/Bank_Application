9ffc7ec3e2c9d0e3692a0c7d1a3d7b9f560f6efd
ercantekoglan <65437249+ercantekoglan@users.noreply.github.com>
2022-03-19 01:09:34 -0500

    Update UserController.java

--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.coderscampus.assignment13.domain.Account;
 import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.service.AccountService;
 import com.coderscampus.assignment13.service.AddressService;
